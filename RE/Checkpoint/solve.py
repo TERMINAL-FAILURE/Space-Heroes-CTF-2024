783514ef2f5913c2506be7b32de38b7fc0798104
Artegium <ethan.n.marshall@protonmail.com>
2024-04-14 19:10:53 -0700

    code solve.py

--- RE/Checkpoint/solve.py
+++ RE/Checkpoint/solve.py
@@ -3,4 +3,17 @@ from pwn import *
 
 io = remote("spaceheroes-checkpoint.chals.io", 443, ssl=True, sni="spaceheroes-checkpoint.chals.io")
 
+
+io.recvuntil(b"Your response: ")
+io.sendline(b"00000000USE")
+
+io.recvuntil(b"Your response: ")
+io.sendline(b"00000000THE")
+
+io.recvuntil(b"Your response: ")
+io.sendline(b"00000000FORCE")
+
+io.recvuntil(b"Your response: ")
+io.sendline(b"00000000LUKE")
+
 io.interactive()
